32e7ed45c675c8219d36439dc640ed70dadf9f43
fake <fake@fake.edu>
2023-08-18 21:56:39 -0500

    update ddp

--- distributed.md
+++ distributed.md
@@ -1,1 +1,4 @@
 # Distributed learning
+
+DDP 
+* make sure a large "timeout" when calling "init_process_group"
--- distributed/ddp.py
+++ distributed/ddp.py
@@ -1,3 +1,4 @@
+# From PyTorch Website https://pytorch.org/tutorials/intermediate/ddp_tutorial.html?utm_source=distr_landing&utm_medium=intermediate_ddp_tutorial
 import os 
 import sys
 import torch
@@ -6,6 +7,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.multiprocessing as mp
 from torch.nn.parallel import DistributedDataParallel as DDP
+import tempfile
 
 def setup(rank, world_size):
     os.environ['MASTER_ADDR'] = 'localhost'
@@ -46,5 +48,32 @@ def run_demo(demo_fn, world_size):
              nprocs = world_size,
              join=True)
 
+def demo_checkpoint(rank, world_size):
+    setup(rank, world_size)
+    model = ToyModel().to(rank)
+    ddp_model = DDP(model, device_ids=[rank])
+
+    CHECKPOINT_PATH = tempfile.gettempdir() + "/model.checkpoint"
+    if rank == 0:
+        torch.save(ddp_model.state_dict(), CHECKPOINT_PATH)
+    
+    dist.barrier()
+    map_location = {"cuda:%d" % 0: 'cuda:%d' % rank}
+    ddp_model.load_state_dict(
+        torch.load(CHECKPOINT_PATH, map_location=map_location)
+    )
+    loss_fn = nn.MSELoss()
+    optimizer = optim.Adam(ddp_model.parameters(), lr=0.001)
+    optimizer.zero_grad()
+    outputs = ddp_model(torch.randn(20,10))
+    labels = torch.randn(20,1).to(rank)
+    loss = loss_fn(outputs, labels)
+    loss.backward()
+    print(loss.item())
+    optimizer.step()
+
+    if rank == 0:
+        os.remove(CHECKPOINT_PATH)
+    cleanup()
 if __name__ =="__main__":
     run_demo(demo_basic, 2)
