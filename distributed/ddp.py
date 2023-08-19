e7baa24e0db0a2a1c1e31403901451c3aae6918c
fake <fake@fake.edu>
2023-08-18 22:21:02 -0500

    update ddp

--- distributed/ddp.py
+++ distributed/ddp.py
@@ -1,4 +1,4 @@
-# From PyTorch Website https://pytorch.org/tutorials/intermediate/ddp_tutorial.html?utm_source=distr_landing&utm_medium=intermediate_ddp_tutorial
+#From https://pytorch.org/tutorials/intermediate/ddp_tutorial.html#skewed-processing-speeds
 import os 
 import sys
 import torch
@@ -11,7 +11,7 @@ import tempfile
 
 def setup(rank, world_size):
     os.environ['MASTER_ADDR'] = 'localhost'
-    os.environ['MASTER_PORT'] = '12355'
+    os.environ['MASTER_PORT'] = '12356'
     dist.init_process_group("gloo", rank=rank, world_size=world_size)
 
 def cleanup():
@@ -42,6 +42,8 @@ def demo_basic(rank, world_size):
 
     cleanup()
 
+
+
 def run_demo(demo_fn, world_size):
     mp.spawn(demo_fn,
              args = (world_size,),
@@ -54,6 +56,7 @@ def demo_checkpoint(rank, world_size):
     ddp_model = DDP(model, device_ids=[rank])
 
     CHECKPOINT_PATH =  "./model.checkpoint"
+    print(rank)
     if rank == 0:
         print("load model")
         torch.save(ddp_model.state_dict(), CHECKPOINT_PATH)
@@ -73,8 +76,40 @@ def demo_checkpoint(rank, world_size):
     print(loss.item())
     optimizer.step()
 
-    if rank == 0:
-        os.remove(CHECKPOINT_PATH)
-    cleanup()
+#     if rank == 0:
+#         os.remove(CHECKPOINT_PATH)
+#     cleanup()
+
+class ToyMpModel(nn.Module):
+    def __init__(self, dev0, dev1):
+        super(ToyMpModel, self).__init__()
+        self.dev0 = dev0
+        self.dev1 = dev1
+        self.net1 = torch.nn.Linear(10, 10).to(dev0)
+        self.relu = torch.nn.ReLU()
+        self.net2 = torch.nn.Linear(10, 1).to(dev1)
+
+    def forward(self, x):
+        x = x.to(self.dev0)
+        x = self.relu(self.net1(x))
+        x = x.to(self.dev1)
+        return self.net2(x)
+
+def demo_model_parallel(rank, world_size):
+    print(f"Running DDP with model parallel")
+    dev0 = rank*2
+    dev1 = rank*2 + 1
+    mp_model = ToyMpModel(dev0, dev1)
+    ddp_mp_model = DDP(mp_model)
+    loss_fn = nn.MSELoss()
+    optimizer = optim.Adam(ddp_mp_model.parameters(), lr=0.001)
+    optimizer.zero_grad()
+    outputs = ddp_model(torch.randn(20,10))
+    labels = torch.randn(20,1).to(dev0)
+    loss = loss_fn(outputs, labels)
+    loss.backward()
+    print(loss.item())
+    optimizer.step()
+
 if __name__ =="__main__":
     run_demo(demo_checkpoint, 2)
